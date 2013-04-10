42f0f081fbf597b0e87ea20a320b376f55121c6f
Arthur Gunn <arthur@gunn.co.nz>
2013-04-10 19:58:45 +1200

    Started blank rails 4rc1 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JSBox::Application.config.secret_key_base = '934b682000f3c1ef8e3af4a57307857e64cc88610922938e4b9089a0f444cc707955f9def6e381d98ebf38b30aa6515004dfab61ddd8a8b477c912a7edc2bdb0'
